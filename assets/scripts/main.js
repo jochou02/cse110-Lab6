281add5256f922b5cb4fbd3fd81a76266af93c51
jochou02 <jochou02@gmail.com>
2022-11-11 10:35:39 -0800

    Forgot to push

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -36,7 +36,7 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
-  mainSection = document.querySelector('main');
+  let mainSection = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
